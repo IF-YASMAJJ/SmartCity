a52ee7db1f65f5024e0379d8e02530dc780ef4ae
feitor <simon.rispal@insa-lyon.fr>
2015-04-28 08:22:37 +0200

    création serveur

---
+++ ServeurSmartCity/ServeurSmartCity/JsonReader/JsonReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using ServeurSmartCity.JsonModel;
+using Newtonsoft.Json;
+
+namespace ServeurSmartCity.JsonReader
+{
+    public class JsonReader
+    {
+
+        private RootObject data;
+        async public void readJson()
+        {
+            HttpClient c = new HttpClient();
+            Uri uri = new Uri("https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&request=GetFeature&typename=sit_sitra.sittourisme");
+
+            String json = await c.GetStringAsync(uri);
+
+            data = JsonConvert.DeserializeObject<RootObject>(json);
+         
+        }
+    }
+}
