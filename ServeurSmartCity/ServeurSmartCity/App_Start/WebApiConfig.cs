279cac4de467d91cf9d9600c87dc17e61a9d4057
marionfaugoin <marion.faugoin@gmail.com>
2015-04-29 11:46:56 +0200

    Renvoi du JSON complet sous la forme API de Jeremy V2

--- ServeurSmartCity/ServeurSmartCity/App_Start/WebApiConfig.cs
+++ ServeurSmartCity/ServeurSmartCity/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using ServeurSmartCity.JsonReader;
 
@@ -20,6 +21,9 @@ namespace ServeurSmartCity
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
             JsonReader.JsonReader json = new JsonReader.JsonReader();
             json.readJson();
         }
