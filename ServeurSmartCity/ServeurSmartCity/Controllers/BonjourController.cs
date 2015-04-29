27dee21bd68f8423c89b391308bc695ddf6306a7
unknown <marion.faugoin@gmail.com>
2015-04-29 09:26:59 +0200

    ajout des fichiers test pour la réponse en xml

---
+++ ServeurSmartCity/ServeurSmartCity/Controllers/BonjourController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ServeurSmartCity.Models;
+
+namespace ServeurSmartCity.Controllers
+{
+    public class BonjourController : ApiController
+    {
+        // Controller simple de test
+
+        // Création des données
+        LieuxTest[] lieux = new LieuxTest[] 
+        { 
+            new LieuxTest { Id = 1, Name = "INSA", Category = "Education", Longitude = 4, Latitude = 45 }, 
+            new LieuxTest { Id = 2, Name = "Bellecour", Category = "Tourisme", Longitude = 5, Latitude = 42 }, 
+            new LieuxTest { Id = 3, Name = "UGC Cite Internationale", Category = "Loisir", Longitude = 3, Latitude = 48 } 
+        };
+
+        // GET : /api/Bonjour
+        public IHttpActionResult GetAllProducts()
+        {
+            return Ok(lieux);
+
+        }
+
+        // GET : /api/Bonjour/"un id"
+        public IHttpActionResult getLieux(int id)
+        {
+            var lieu = lieux.FirstOrDefault((p) => p.Id == id);
+            if (lieu == null)
+            {
+                return NotFound();
+            }
+            return Ok(lieu);
+        }
+    }
+}
---
+++ ServeurSmartCity/ServeurSmartCity/Models/LieuxTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServeurSmartCity.Models
+{
+    public class LieuxTest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+}
