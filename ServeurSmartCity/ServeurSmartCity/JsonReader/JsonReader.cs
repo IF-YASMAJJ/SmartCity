79b0bf61dd8b0aaea4335aaaefeaf879a87523c8
jeje42 <jerome.guidon@insa-lyon.fr>
2015-04-29 17:19:43 +0200

    Avancement classification (not finished yet)

--- ServeurSmartCity/ServeurSmartCity/JsonReader/JsonReader.cs
+++ ServeurSmartCity/ServeurSmartCity/JsonReader/JsonReader.cs
@@ -15,6 +15,9 @@ namespace ServeurSmartCity.JsonReader
     {
 
         private RootObject data;
+        private float   minLat = (float)90.0D, maxLat = (float)0.0D, 
+                        minLong = (float)180.0D, maxLong = (float)-180.0D;
+
         async public void readJson()
         {
             HttpClient c = new HttpClient();
@@ -23,6 +26,30 @@ namespace ServeurSmartCity.JsonReader
             String json = await c.GetStringAsync(uri);
 
             data = JsonConvert.DeserializeObject<RootObject>(json);
+            float longBuf;
+            float latBuf;
+            foreach (Feature f in data.features)
+            {
+                longBuf = (float)f.geometry.coordinates[0];
+                latBuf = (float)f.geometry.coordinates[1];
+                
+                if (longBuf < minLong)  minLong = longBuf;
+                if (longBuf > maxLong)  maxLong = longBuf;
+                if (latBuf < minLat) minLat = latBuf;
+                if (latBuf > maxLat) maxLat = latBuf;
+            }
+
+            //http://blogs.msdn.com/b/ogdifrance/archive/2011/07/13/de-la-g-233-o-et-des-maths.aspx
+            var latitude1 = minLat * Math.PI/180;
+            var latitude2 = maxLat * Math.PI / 180;
+            var longitude1 = minLong * Math.PI / 180;
+            var longitude2 = maxLong * Math.PI / 180;
+            var R = 6371d;
+            //d : distance en kilomètres entre les points extremums du domaine (testé)
+            //TODO : définir la taille des carrés
+            var d = R * Math.Acos(Math.Cos(latitude1) * Math.Cos(latitude2) *
+                    Math.Cos(longitude2 - longitude1) + Math.Sin(latitude1) *
+                             Math.Sin(latitude2));
 
             editModel();
         }
